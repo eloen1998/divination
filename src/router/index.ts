bd4e5a400826b69dc4298351fd90aa54c9bbd770
eloen <597962307@qq.com>
2023-06-09 18:31:24 +0800

    fix: 修复部署问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const About = { template: '<div>About</div>' }
-
 const routes = [
-  { path: '/', component: import('../views/home.vue') },
-  { path: '/game', component: import('../views/game.vue') },
-  { path: '/solve/:id', component: import('../views/solve.vue') },
-  { path: '/about', component: About }
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  { path: '/home', component: () => import('../views/home.vue') },
+  { path: '/game', component: () => import('../views/game.vue') },
+  { path: '/solve/:id', component: () => import('../views/solve.vue') }
 ]
 
 export default createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHistory(),
-  routes // `routes: routes` 的缩写
+  routes
 })
